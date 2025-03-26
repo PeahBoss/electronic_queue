31598fd14a17c94a4d945bd179308f5786ffb00a
Алина Михович <mikhovichaa@gmail.com>
2025-03-26 22:53:38 +0300

    Добавление сущности Specialization

---
+++ core/Entities/Specialization.cs
@@ -0,0 +1,11 @@
+namespace Library.Core.Entities;
+
+public class Specialization : IEntity 
+{
+    public Id Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+    public virtual ICollection<doctors> doctor { get; set; } = [];
+
+
+}
